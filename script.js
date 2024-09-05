6284f2d3343c2818c8b5be247f7110b4d33f8ec3
Garv Manchanda <156130122+garvmanc@users.noreply.github.com>
2024-09-06 03:04:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+new TradingView.widget({
+    "width": "980",
+    "height": "610",
+    "symbol": "NSE:RELIANCE",
+    "interval": "D",
+    "timezone": "Etc/UTC",
+    "theme": "dark",
+    "style": "5",
+    "locale": "en",
+    "hide_top_toolbar": true,
+    "hide_legend": true,
+    "allow_symbol_change": false,
+    "save_image": false,
+    "calendar": false,
+    "hide_volume": true,
+    "support_host": "https://www.tradingview.com"
+});
